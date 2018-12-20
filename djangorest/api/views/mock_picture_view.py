3c1ea9b68c678e07b459e2a5a04b638633c1acd4
grantzhang <grantzhang@objectivasoftware.com>
2018-12-20 17:13:03 +0800

    simple

--- djangorest/api/views/mock_picture_view.py
+++ djangorest/api/views/mock_picture_view.py
@@ -1,14 +1,5 @@
-from rest_framework.response  import Response
-from rest_framework.request  import Request
-from rest_framework.renderers import JSONRenderer
-from io import BytesIO
 from django.http import HttpResponse,HttpRequest
-import os
-import pickle
-from datetime import datetime
-from datetime import timedelta
 from api.drawgraphs.mock_graph import draw_mock_graph
-from urllib.parse import urlparse, parse_qs
 from .view import KMCGraph
 
 class MockPicture(KMCGraph):
@@ -25,8 +16,3 @@ class MockPicture(KMCGraph):
         pil_image.save(response,"PNG")
 
         return response
-
-    def debug_graph(self, url):
-        
-        parameters = {'locale':'en'}
-        return self.draw_graph(parameters, True)
--- djangorest/api/views/view.py
+++ djangorest/api/views/view.py
@@ -8,14 +8,3 @@ class KMCGraph(APIView):
         '''
             draw the graph and return a image response.
         '''
-    
-    def draw_graph(self, parameters, isdebug = False):
-        '''
-            parameters is a dictionary. one parameter will always be locale that defaults to 'en'.
-            draw the graph and return a image response.
-        '''
-
-    def debug_graph(self, url):
-        '''
-            It is uesed to debug locally. It takes a url and calls the draw_graph method with the parameters from the url.
-        '''
